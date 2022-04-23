745ee207d9268c39a15bbab1d5b57a039848a6b7
Fernandeeess <82266442+Fernandeeess@users.noreply.github.com>
2022-04-23 04:31:43 -0300

    correcao de problemas

--- index.py
+++ index.py
@@ -1,28 +1,34 @@
-print("Sistema de conversação de base decimal para binario, hexadecimal, octal")
-print("[1] para binario")
-print("[2] para hexadecimal")
-print("[3] para octal")
-selected = int(input("Selecione uma das opções acima, ex(1,2,3): "))
+converter = True;
+def continuarOuNao():
+    
+    continuar = str(input("Se quiser continuar convertendo digite [Y] "))
+    if (continuar == "Y" or continuar == "y"):
+        converter = True
+    else:
+        print("valor invalido")
+
 
 def decToBin():
     print("Decimal/Binario")
     valor = int(input("Digite o valor em decimal: "))
-    resultado = "" 
+    resultado = ""
     while(valor != 0 or valor < 0):
-            resto = valor % 2
-            valor = valor // 2
-            resultado = resultado + str(resto);      
+        resto = valor % 2
+        valor = valor // 2
+        resultado = resultado + str(resto)
     # resultado.reverse()
-    print(resultado[::-1])
+    print(resultado[::-1], "Binario")
+    continuarOuNao()
+
 
 def decToHexa():
     print("Decimal/Hexadecimal")
     valor = int(input("Digite o valor em decimal: "))
-    resultado = "" 
+    resultado = ""
     while(valor != 0 or valor < 0):
         resto = valor % 16
         valor = valor // 16
-        if(resto > 9 and resto< 16):
+        if(resto > 9 and resto < 16):
             if resto == 10:
                 resto = "A"
             elif resto == 11:
@@ -35,32 +41,43 @@ def decToHexa():
                 resto = "E"
             elif resto == 15:
                 resto = "F"
-            
-        resultado = resultado + str(resto);
-    
-    print(resultado[::-1]);
+
+        resultado = resultado + str(resto)
+
+    print(resultado[::-1], "Hexadecimal")
+    continuarOuNao()
+
 
 def decToOctal():
     print("Decimal/Octal")
     valor = int(input("Digite o valor em decimal: "))
-    resultado = "" 
+    resultado = ""
     while(valor != 0 or valor < 0):
-            resto = valor % 8
-            valor = valor // 8
-            resultado = resultado + str(resto);      
+        resto = valor % 8
+        valor = valor // 8
+        resultado = resultado + str(resto)
     # resultado.reverse()
-    print(resultado[::-1])
+    print(resultado[::-1], "Octal")
+    continuarOuNao()
 
 
-if (selected == 1):
-    decToBin()
-elif (selected == 2):
-    print("decimal/hexadecimal")
-    decToHexa()
-elif (selected == 3):
-    print("decimal/octodecimal")
-    decToOctal()
-else:
-    print("nenhuma da opções anteriores foram selecionadas")
+while converter == True:
+    print("")
+    print("Sistema de conversação de base decimal para binario, hexadecimal, octal")
+    print("")
+    print("[1] para binario")
+    print("")
+    print("[2] para hexadecimal")
+    print("")
+    print("[3] para octal")
+    print("")
+    selected = int(input("Selecione uma das opções acima, ex(1,2,3): "))
 
-
+    if (selected == 1):
+        decToBin()
+    elif (selected == 2):
+        decToHexa()
+    elif (selected == 3):
+        decToOctal()
+    else:
+        print("nenhuma da opções anteriores foram selecionadas")
